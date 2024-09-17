a12e4dec2f4b81cbcbd1e7c40d6e691c367d7817
Murinello <pedro.pm.vale@gmail.com>
2024-09-17 14:31:43 +0200

    Initial commit from create-remix

---
+++ src/routes/auth.auth0.tsx
@@ -0,0 +1,7 @@
+import type { ActionFunction } from '@remix-run/node';
+import { authenticator } from '~/auth.server';
+
+export const action: ActionFunction = () => {
+  authenticator.authorize();
+  return null;
+};
